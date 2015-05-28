058da9a3c3e9b04fdc71de2fa12d2eac93d87493
Bhavish Yalamanchi <bhavishy6@gmail.com>
2015-05-28 15:29:59 -0400

    typos

--- js/menuscrolling.js
+++ js/menuscrolling.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
         var nav_top = $(".sidebar-nav").scrollTop();
     });
 
-    $(".sidebar-nav a").click(function(env) {
-        evn.preventDefault();
+    $(".sidebar-nav li a").click(function(env) {
+        env.preventDefault();
         $("html, body").scrollTo(this.hash, this.hash);
     });
 });
