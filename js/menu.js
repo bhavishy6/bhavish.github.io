f902273805f3747ad2b7e18717f632f5c9055713
Bhavish Yalamanchi <bhavishy6@gmail.com>
2015-05-28 14:40:45 -0400

    js fixes

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,15 @@
 $(function() {
-    var $sidebar = $("#sidebar-nav"),
-        $window = $(window),
-        offset = $sidebar.offset(),
-        topPadding = 15;
+    var offset = $("sidebar-nav").offset();
+    var topPadding = 15;
 
-    $window.scroll(function() {
-        if ($window.scrollTop() < offset.top) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > offset.top) {
+            $("sidebar-nav").stop().animate({
+                marginTop: $(window).scrollTop() - offset.top + topPadding
             });
         } else {
-            $sidebar.stop().animate({
-                marginTop: 0;
+            $("sidebar-nav").stop().animate({
+                marginTop: 0
             });
         }
     });
